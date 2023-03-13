46dc88f3537234ea4ba9a3d78c92abc8ba2ccc7a
kisapissa <ksiruk334@gmail.com>
2023-03-13 14:00:01 +0200

    Improve transaction batching efficiency

--- proposal_handler.py
+++ proposal_handler.py
@@ -2,3 +2,4 @@
 // Add support for decentralized credit scoring
 // Improve decentralized identity (DID) systems
 // Improve contract execution speed
+// Add support for zk-SNARKs in smart contracts
