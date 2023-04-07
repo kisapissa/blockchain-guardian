bf31d7a4fffec276168701f48704e8b2e42d82ba
kisapissa <ksiruk334@gmail.com>
2023-04-07 14:00:02 +0200

    Add batch transaction capabilities

--- voting_mechanism.go
+++ voting_mechanism.go
@@ -1,3 +1,4 @@
 // Integrate Chainlink VRF for randomness
 // Refactor reward pooling algorithm
 // Fix interoperability issues with other blockchains
+// Optimize decentralized application performance
