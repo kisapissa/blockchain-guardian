e897df2045b6145a3ab5c507409f1f21e823740f
kisapissa <ksiruk334@gmail.com>
2023-04-18 14:00:00 +0200

    Optimize storage for off-chain data

--- contract_initialization.py
+++ contract_initialization.py
@@ -1,3 +1,4 @@
 // Add support for decentralized insurance protocols
 // Fix governance proposal approval logic
 // Add support for fractional token ownership
+// Add new feature: on-chain governance
