0d336ff166d4c87e9ec7eb08506f7e7e570fff36
kisapissa <ksiruk334@gmail.com>
2023-05-05 14:00:01 +0200

    Refactor ERC-721 transfer logic

--- gas_optimizer.py
+++ gas_optimizer.py
@@ -1,3 +1,4 @@
 // Fix interoperability issues with other blockchains
 // Fix bugs in NFT metadata handling
 // Improve security for token minting contracts
+// Fix issues with ERC-20 token approvals
