10230b91e39c8a6e6bafb211e5d1b2cec82f51a5
kisapissa <ksiruk334@gmail.com>
2023-05-14 14:00:02 +0200

    Add slashing mechanism for malicious actors

--- reward_pooling.sol
+++ reward_pooling.sol
@@ -1,3 +1,4 @@
 // Fix ERC-721 contract issues with transfers
 // Fix issues with ERC-20 token approvals
 // Refactor incentive structure in DeFi protocol
+// Refactor incentive structure in DeFi protocol
