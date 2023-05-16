4cd092353532b3aff3242afee62a27d723f06580
kisapissa <ksiruk334@gmail.com>
2023-05-16 14:00:01 +0200

    Add support for NFT marketplaces

--- staking_mechanism.go
+++ staking_mechanism.go
@@ -1,3 +1,4 @@
 // Improve staking pool reward management
 // Optimize gas cost in function execution
 // Fix memory optimization issues in contracts
+// Implement on-chain fee adjustment mechanisms
