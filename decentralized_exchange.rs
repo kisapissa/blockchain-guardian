d4531d28bc2628ab5ab8fc727d944aa4a9cd1a73
kisapissa <ksiruk334@gmail.com>
2023-06-29 14:00:00 +0200

    Integrate with external DeFi protocols

--- decentralized_exchange.rs
+++ decentralized_exchange.rs
@@ -1,3 +1,4 @@
 // Add support for multi-signature wallet contracts
 // Improve staking logic for better rewards
 // Refactor smart contract testing framework
+// Integrate Chainlink oracles for price data
