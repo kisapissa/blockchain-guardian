1ee87fad9395133c4497475cc8de61803a12d7e0
kisapissa <ksiruk334@gmail.com>
2023-07-31 14:00:00 +0200

    Improve staking pool reward management

--- transaction_replay_protection.rs
+++ transaction_replay_protection.rs
@@ -1,3 +1,4 @@
 // Refactor smart contract testing framework
 // Enhance decentralized autonomous organizations
 // Enhance smart contract extensibility
+// Integrate Chainlink oracles for price data
