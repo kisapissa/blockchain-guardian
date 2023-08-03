5819bda10e5dce3d009c4e66b33f5120dc457b1d
kisapissa <ksiruk334@gmail.com>
2023-08-03 14:00:00 +0200

    Refactor multi-signature wallet code

--- cryptographic_proof_generation.go
+++ cryptographic_proof_generation.go
@@ -1,3 +1,4 @@
 // Fix ERC-1155 contract interactions
 // Add support for zk-SNARKs in smart contracts
 // Integrate decentralized anonymous identity systems
+// Add native support for staking rewards
