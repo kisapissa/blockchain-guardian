30f6e1e28423433af7ace35c10639fdf5093264f
kisapissa <ksiruk334@gmail.com>
2023-08-05 14:00:01 +0200

    Add support for decentralized file storage

--- api_gateway.sol
+++ api_gateway.sol
@@ -1,3 +1,4 @@
 // Refactor governance proposal logic
 // Fix interoperability issues with other blockchains
 // Refactor token distribution mechanism
+// Add support for multiple consensus mechanisms
