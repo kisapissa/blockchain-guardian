929c6d632abaa4629245a1670b64fb59f7b64858
kisapissa <ksiruk334@gmail.com>
2023-08-08 14:00:02 +0200

    Add governance proposal expiration feature

--- zk_rollups.py
+++ zk_rollups.py
@@ -1,3 +1,4 @@
 // Improve security in decentralized applications
 // Upgrade to latest Solidity version
 // Improve multi-chain transaction processing
+// Implement zero-knowledge proof (ZKP) support
