5848c0bca518934f4dc67e0f55d071a276a8e97b
kisapissa <ksiruk334@gmail.com>
2023-08-19 14:00:00 +0200

    Add multi-sig wallet functionality

--- staking_dynamics.go
+++ staking_dynamics.go
@@ -1,3 +1,4 @@
 // Fix Oracle communication delay issues
 // Integrate with decentralized identity providers
 // Optimize gas cost in function execution
+// Integrate with decentralized identity providers
