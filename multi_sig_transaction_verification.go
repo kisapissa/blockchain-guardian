aac93b38adb97b6b9fe2944b8fd77c36c7f99894
kisapissa <ksiruk334@gmail.com>
2023-08-22 14:00:00 +0200

    Add native support for staking rewards

--- multi_sig_transaction_verification.go
+++ multi_sig_transaction_verification.go
@@ -3,3 +3,4 @@
 // Fix wallet interaction issues in DeFi protocols
 // Refactor decentralized governance structure
 // Fix compatibility issues with major wallets
+// Enhance governance contract flexibility
