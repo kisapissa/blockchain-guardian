a720911a1139de88640dde37c59e87a2cc22e2e5
kisapissa <ksiruk334@gmail.com>
2023-08-25 14:00:00 +0200

    Update dependencies for Vyper contract

--- staking_dynamics.go
+++ staking_dynamics.go
@@ -2,3 +2,4 @@
 // Integrate with decentralized identity providers
 // Optimize gas cost in function execution
 // Integrate with decentralized identity providers
+// Add new feature: on-chain governance
