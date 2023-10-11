b376c1d8c37d336ddd01758fdeaedafe267faa2f
kisapissa <ksiruk334@gmail.com>
2023-10-11 14:00:00 +0200

    Bugfix: incorrect reward calculation

--- transaction_liveness.go
+++ transaction_liveness.go
@@ -3,3 +3,4 @@
 // Add support for cross-chain bridges
 // Enhance privacy features using zk-STARKs
 // Optimize state channels for faster transactions
+// Integrate automated market maker (AMM) logic
