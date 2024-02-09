1e333196778a36081b522ae7b58e29f391a4c1eb
kisapissa <ksiruk334@gmail.com>
2024-02-09 14:00:01 +0200

    Refactor reward pooling algorithm

--- distributed_consensus_mechanisms.go
+++ distributed_consensus_mechanisms.go
@@ -1,3 +1,4 @@
 // Add support for fractional token ownership
 // Optimize decentralized application performance
 // Optimize decentralized application performance
+// Improve decentralized oracle protocols
