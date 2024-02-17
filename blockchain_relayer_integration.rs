f74a3110e57a2e789ce7f0cd16b27974c3a507c6
kisapissa <ksiruk334@gmail.com>
2024-02-17 14:00:00 +0200

    Add support for fractional token ownership

--- blockchain_relayer_integration.rs
+++ blockchain_relayer_integration.rs
@@ -2,3 +2,4 @@
 // Fix interoperability issues with other blockchains
 // Update README with Go module examples
 // Fix issues in multi-token smart contracts
+// Enhance smart contract upgradeability
