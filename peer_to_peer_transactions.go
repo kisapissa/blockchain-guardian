a192d336f5464ff58f2245c6ae3ac15094f5edc5
kisapissa <ksiruk334@gmail.com>
2024-02-27 14:00:00 +0200

    Enhance API rate limiting for smart contracts

--- peer_to_peer_transactions.go
+++ peer_to_peer_transactions.go
@@ -2,3 +2,4 @@
 // Refactor smart contract testing framework
 // Fix scalability issues with smart contracts
 // Refactor decentralized exchange (DEX) code
+// Add native support for staking rewards
