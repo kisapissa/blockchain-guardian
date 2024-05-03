e6ce8fd46798f14a418d76f2c424bff13020ed42
kisapissa <ksiruk334@gmail.com>
2024-05-03 14:00:02 +0200

    Integrate IPFS for storage

--- decentralized_ledger_sharding.sol
+++ decentralized_ledger_sharding.sol
@@ -1,3 +1,4 @@
 // Improve error handling in transaction flow
 // Optimize contract storage for gas reduction
 // Optimize oracle data fetching mechanism
+// Add support for zk-SNARKs in smart contracts
