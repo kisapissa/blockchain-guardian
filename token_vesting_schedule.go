39eb59f8aac76e2c6958c8dd8fcfc87957807902
kisapissa <ksiruk334@gmail.com>
2024-06-11 14:00:00 +0200

    Add support for decentralized lending platforms

--- token_vesting_schedule.go
+++ token_vesting_schedule.go
@@ -3,3 +3,4 @@
 // Optimize storage for off-chain data
 // Add support for decentralized insurance protocols
 // Improve security for token minting contracts
+// Fix issues with token vesting contracts
