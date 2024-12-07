bdd56af22d018075236492c611419f25367647dd
kisapissa <ksiruk334@gmail.com>
2024-12-07 14:00:00 +0200

    Fix overflow bug in token calculation

--- staking_reward_calculation.rs
+++ staking_reward_calculation.rs
@@ -4,3 +4,4 @@
 // Refactor incentive structure in DeFi protocol
 // Add automated contract testing tools
 // Integrate decentralized anonymous identity systems
+// Add support for zk-SNARKs in smart contracts
